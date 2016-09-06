291bb97275e3daf2f91c56be785e24a0b5735479
christinekim1 <kimchristine8@gmail.com>
2016-09-06 12:14:42 -0700

    Added Files

---
+++ APLesson_01/Lab_01.java
@@ -0,0 +1,32 @@
+public class secondprog
+{
+	public static void main (String[]args)
+	{ 
+		System.out.println("                   dMP dMP  .aMMMb   dMMMMb   dMMMMb   dMP dMP");
+		System.out.println("                  dMP dMP  dMP\"dMP  dMP.dMP  dMP.dMP  dMP.dMP");
+		System.out.println("                 dMMMMMP  dMMMMMP  dMMMMP\"  dMMMMP\"   VMMMMP");
+		System.out.println("                dMP dMP  dMP dMP  dMP      dMP      dA .dMP");
+		System.out.println("               dMP dMP  dMP dMP  dMP      dMP       VMMMP\"");  
+		System.out.println("      dMMMMb   dMP  dMMMMb  dMMMMMMP  dMP dMP  dMMMMb  .aMMMb   dMP dMP");
+		System.out.println("     dMP\"dMP  amr  dMP.dMP    dMP    dMP dMP  dMP VMP  dMP\"dMP  dMP.dMP");  
+		System.out.println("    dMMMMK\"  dMP  dMMMMK\"    dMP    dMMMMMP  dMP dMP  dMMMMMP   VMMMMP\"");  
+		System.out.println("   dMP.aMF  dMP  dMP\"AMF    dMP    dMP dMP  dMP.aMP  dMP dMP  dA .dMP"); 
+		System.out.println("  dMMMMP\"  dMP  dMP dMP    dMP    dMP dMP  dMMMMP\"  dMP dMP   VMMMP\"");
+		
+		System.out.println("\n");
+	
+		System.out.println("¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶");
+		System.out.println("¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶");
+		System.out.println("¶¶¶¶¶¶¶1¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶§§§1§§¶¶");
+		System.out.println("¶¶¶¶¶¶_§¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶§1___1§§¶¶¶¶¶");
+		System.out.println("¶¶¶¶§__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶§1______§¶¶¶¶¶¶¶¶¶¶");
+		System.out.println("¶¶¶§___¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶§§1_______1§¶¶¶¶¶¶¶¶¶¶¶¶¶¶");
+		System.out.println("¶¶¶____1¶¶¶¶¶¶¶¶¶¶§11_________1¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶");
+		System.out.println("¶¶_______11111____________§¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶");
+		System.out.println("¶¶____________________§¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶");
+		System.out.println("¶¶_______________1§¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶");
+		System.out.println("¶¶¶__________1§¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶");
+		System.out.println("¶¶¶¶¶¶§§§¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶");
+
+	}
+}
