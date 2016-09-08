0cb914046461822e5ffbab189d4b15052e1886b9
christinekim1 <kimchristine8@gmail.com>
2016-09-08 11:08:31 -0700

    Added Files

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,35 @@
+public class Lab_02
+{
+	public static void main (String[]args)
+	{
+		int x = 2;
+		int y = 4;
+		int multiply= x*y;
+		System.out.println("2 multiplied by 4 is " + multiply );
+		
+		System.out.println("\n");
+		
+		String name = "Barack Obama";
+		System.out.println(name);
+		String address = "1600 Pennsylvania Avenue";
+		System.out.println(address);
+		String city = "Washington DC";
+		System.out.println(city);
+		String zip = "20500";
+		System.out.println(zip);
+		
+		System.out.println("\n");
+		
+		int l = 3;
+		int w = 5;
+		int h = 6;
+		int A1 = l*w;
+		int A2 = w*h;
+		int A3 = h*l;
+		int surfArea = 2*(A1 + A2 + A3);
+		System.out.println( "The surface area of your rectangle is " + surfArea);
+		
+		
+		
+	}
+}
