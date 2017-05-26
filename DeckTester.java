8ba25d3801be582d36a7ec4f3c7327b6a72dec0b
christinekim1 <kimchristine8@gmail.com>
2017-05-26 11:26:56 -0700

    Update DeckTester.java

--- DeckTester.java
+++ DeckTester.java
@@ -9,5 +9,14 @@ public class DeckTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		String[] rank = "10", "Ace", "5", "Queen";
+		String[] suit = "Spade", "Clover", "Diamond", "Hearts";
+		int[] value = {15, 20, 25, 30};
+		Deck cards = Deck(rank, suit, value);
+
+		System.out.println("Your cards are: " + cards.deal());
+		System.out.println("The deck size is: " + cards.size());
+		System.out.println("The deck is empty: " + cards.isEmpty());
+
 	}
 }
